100dd419d415485eaafaa21f03651a174b38b8e2
jeffconexion <jeff.debian2016@gmail.com>
2026-02-02 22:48:00 -0300

    Refactor Angular components and webpack configurations
    - Removed unused RouterOutlet imports from app.ts files in grafico and produtos.
    - Updated webpack.config.js files in grafico, produtos, and vendas for improved readability and consistency.
    - Corrected file path separators in exposes section of webpack configurations.
    - Enhanced shared configuration in webpack to maintain consistency across modules.
    - Simplified app.html in produtos by replacing placeholder content with a header.

--- vendas/webpack.config.js
+++ vendas/webpack.config.js
@@ -1,13 +1,19 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
+    produtos: 'http://localhost:4201/remoteEntry.js',
+    grafico: 'http://localhost:4202/remoteEntry.js',
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
   },
-
 });
